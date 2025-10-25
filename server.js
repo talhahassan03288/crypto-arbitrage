504dedb67265dfa1ff77b997f27755dc47851abb
talhahassan03288 <talhahassan03288@gmail.com>
2025-10-25 20:43:21 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+{
+  "name": "crypto-arbitrage",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "axios": "^1.7.5",
+    "express": "^4.18.2",
+    "node-telegram-bot-api": "^0.61.0"
+  }
+}
